af1255f1cf6b4944ad071e6560a5c60aa9055306
RikardLundberg <rikardlundberg90@gmail.com>
2023-12-04 08:16:25 +0100

    day_4, second star

--- src/day_4/main.cpp
+++ src/day_4/main.cpp
@@ -34,6 +34,7 @@ vector<int> getSortedNumberSplit(const string &str)
     if (token != "" && isOnlyInt(token))
         nbrs.push_back(stoi(token));
     sort(nbrs.begin(), nbrs.end());
+    return nbrs;
 }
 
 int main(void)
@@ -54,6 +55,7 @@ int firstStar()
         int previous = -1;
         for (int i = 0; i < nbrs.size(); i++)
         {
+            int hej = nbrs[i];
             if (nbrs[i] == previous)
             {
                 if (addTotal == 0)
@@ -68,6 +70,44 @@ int firstStar()
     cout << "1st total: " << total << endl;
 }
 
+int countMatches(const vector<int>& nbrs)
+{
+    int previous = -1;
+    int matches = 0;
+    for (int i = 0; i < nbrs.size(); i++)
+    {
+        if (nbrs[i] == previous)
+            matches++;
+        previous = nbrs[i];
+    }
+    return matches;
+}
+
 int secondStar()
 {
+    ifstream infile("input");
+    string line;
+    vector<int> scratchCards;
+    int currentCard = 0;
+    while (getline(infile, line))
+    {
+        vector<int> nbrs = getSortedNumberSplit(line);
+        int matches = countMatches(nbrs);
+        int factor = 1;
+        if(scratchCards.size() > currentCard)
+            factor += scratchCards[currentCard];
+
+        while(scratchCards.size() < currentCard + matches + 1)
+            scratchCards.push_back(0);
+
+        for(int i = currentCard + 1; i < currentCard + matches + 1; i++)
+            scratchCards[i] += factor;
+
+        currentCard++;
+    }
+
+    int total = 0;
+    for(int i = 0; i < scratchCards.size(); i++)
+        total += scratchCards[i] + 1;
+    cout << "2nd total: " << total << endl;
 }
