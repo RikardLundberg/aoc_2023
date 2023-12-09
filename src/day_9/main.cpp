a7956aff8801cb6ddd9a5a3fecf24d2e80d917b6
Rikard Lundberg <Rikard.Lundberg@symetri.com>
2023-12-09 19:01:56 +0100

    day_9, second star

--- src/day_9/main.cpp
+++ src/day_9/main.cpp
@@ -32,6 +32,8 @@ int main()
     secondStar();
 }
 
+vector<int> supposedSizes;
+
 void firstStar()
 {
     long long total = 0;
@@ -40,7 +42,7 @@ void firstStar()
         vector<int> currentInput = inputs[i];
         vector<int> lastValueHist;
         lastValueHist.push_back(currentInput.back());
-        
+
         while (lastValueHist.back() != 0)
         {
             vector<int> iteration;
@@ -56,6 +58,7 @@ void firstStar()
                 currentInput.push_back(itrVal);
         }
 
+        supposedSizes.push_back(lastValueHist.size());
         for (int lastVal : lastValueHist)
             total += lastVal;
     }
@@ -64,5 +67,34 @@ void firstStar()
 
 void secondStar()
 {
+    long long total = 0;
+    for (int i = 0; i < inputs.size(); i++)
+    {
+        vector<int> currentInput = inputs[i];
+        reverse(currentInput.begin(), currentInput.end());
+
+        vector<int> lastValueHist;
+        lastValueHist.push_back(currentInput.back());
+
+        while (lastValueHist.size() < supposedSizes[i])
+        {
+            vector<int> iteration;
+            int previous = currentInput[0];
+            for (int j = 1; j < currentInput.size(); j++)
+            {
+                iteration.push_back(currentInput[j] - previous);
+                previous = currentInput[j];
+            }
+            lastValueHist.push_back(iteration.back());
+            currentInput.clear();
+            for (int itrVal : iteration)
+                currentInput.push_back(itrVal);
+        }
+
+        supposedSizes.push_back(lastValueHist.size());
+        for (int lastVal : lastValueHist)
+            total += lastVal;
+    }
+    cout << "2nd : " << total << endl;
 
 }
